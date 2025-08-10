ec9d19853acf7604a7c55b5bb15d9afba5af993c
jgtech1205 <jgtech1205@gmail.com>
2025-08-10 13:20:59 -0600

    fixed error

--- api/test.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({
-    message: 'Test function working!',
-    timestamp: new Date().toISOString()
-  });
-}; 
--- hello.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({ 
-    message: 'Hello from root function!',
-    timestamp: new Date().toISOString()
-  });
-} 
